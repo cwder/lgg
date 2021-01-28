f26a74355b945764191bd2c4dfd1f883c16c1a34
chinapnr <wenda.cao@huifu.com>
2021-01-29 00:07:07 +0800

    okhttp

--- app/src/main/java/com/example/lgg/tt/HttpUrl.java
+++ app/src/main/java/com/example/lgg/tt/HttpUrl.java
@@ -1,8 +1,14 @@
 package com.example.lgg.tt;
 
+import android.text.TextUtils;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.annotation.Nullable;
+import okhttp3.internal.Util;
+import okio.Buffer;
 
 public final class HttpUrl {
     private static final char[] HEX_DIGITS =
@@ -49,7 +55,148 @@ public final class HttpUrl {
 
         int port = -1;
 
+        final List<String> encodedPathSegments = new ArrayList<>();
+
+        @Nullable List<String> encodedQueryNamesAndValues;
+
+        @Nullable String encodedFragment;
+
+        Builder(){
+            encodedPathSegments.add("");
+        }
+
+        public Builder scheme(String scheme){
+            if (scheme == null){
+                throw new NullPointerException("scheme == null");
+            }else if(scheme.equalsIgnoreCase("http")){
+                this.scheme = "http";
+            }else if(scheme.equalsIgnoreCase("https")){
+                this.scheme = "https";
+            }else{
+                throw new IllegalArgumentException("unexpected scheme: " + scheme);
+            }
+
+            return this;
+
+        }
+
+        public Builder username(String username){
+            if (username == null){
+                throw new NullPointerException("username == null");
+            }
+
+            this.encodedUsername = canonicalize(username, USERNAME_ENCODE_SET, false, false, false, true);
+            return this;
+        }
+
+        public Builder encodedUsername(){
+            if (encodedUsername == null){
+                throw new NullPointerException("encodedUsername == null");
+            }
+
+            this.encodedUsername = canonicalize(
+                    encodedUsername, USERNAME_ENCODE_SET, true, false, false, true);
+
+            return this;
+        }
+
+        public Builder password(String password) {
+            if (password == null) throw new NullPointerException("password == null");
+            this.encodedPassword = canonicalize(password, PASSWORD_ENCODE_SET, false, false, false, true);
+            return this;
+        }
+
+        public Builder encodedPassword(String encodedPassword) {
+            if (encodedPassword == null) throw new NullPointerException("encodedPassword == null");
+            this.encodedPassword = canonicalize(
+                    encodedPassword, PASSWORD_ENCODE_SET, true, false, false, true);
+            return this;
+        }
+
+
+
     }
 
+
+
+    static String canonicalize(String input, int pos, int limit, String encodeSet,
+                               boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly,
+                               Charset charset) {
+        int codePoint;
+        for (int i = pos; i < limit; i += Character.charCount(codePoint)) {
+            codePoint = input.codePointAt(i);
+            if (codePoint < 0x20
+                    || codePoint == 0x7f
+                    || codePoint >= 0x80 && asciiOnly
+                    || encodeSet.indexOf(codePoint) != -1
+                    || codePoint == '%' && (!alreadyEncoded || strict && !percentEncoded(input, i, limit))
+                    || codePoint == '+' && plusIsSpace) {
+                // Slow path: the character at i requires encoding!
+                Buffer out = new Buffer();
+                out.writeUtf8(input, pos, i);
+                canonicalize(out, input, i, limit, encodeSet, alreadyEncoded, strict, plusIsSpace,
+                        asciiOnly, charset);
+                return out.readUtf8();
+            }
+        }
+
+        // Fast path: no characters in [pos..limit) required encoding.
+        return input.substring(pos, limit);
+    }
+
+    static void canonicalize(Buffer out, String input, int pos, int limit, String encodeSet,
+                             boolean alreadyEncoded, boolean strict, boolean plusIsSpace, boolean asciiOnly,
+                             Charset charset) {
+        Buffer encodedCharBuffer = null; // Lazily allocated.
+        int codePoint;
+        for (int i = pos; i < limit; i += Character.charCount(codePoint)) {
+            codePoint = input.codePointAt(i);
+            if (alreadyEncoded
+                    && (codePoint == '\t' || codePoint == '\n' || codePoint == '\f' || codePoint == '\r')) {
+                // Skip this character.
+            } else if (codePoint == '+' && plusIsSpace) {
+                // Encode '+' as '%2B' since we permit ' ' to be encoded as either '+' or '%20'.
+                out.writeUtf8(alreadyEncoded ? "+" : "%2B");
+            } else if (codePoint < 0x20
+                    || codePoint == 0x7f
+                    || codePoint >= 0x80 && asciiOnly
+                    || encodeSet.indexOf(codePoint) != -1
+                    || codePoint == '%' && (!alreadyEncoded || strict && !percentEncoded(input, i, limit))) {
+                // Percent encode this character.
+                if (encodedCharBuffer == null) {
+                    encodedCharBuffer = new Buffer();
+                }
+
+                if (charset == null || charset.equals(Util.UTF_8)) {
+                    encodedCharBuffer.writeUtf8CodePoint(codePoint);
+                } else {
+                    encodedCharBuffer.writeString(input, i, i + Character.charCount(codePoint), charset);
+                }
+
+                while (!encodedCharBuffer.exhausted()) {
+                    int b = encodedCharBuffer.readByte() & 0xff;
+                    out.writeByte('%');
+                    out.writeByte(HEX_DIGITS[(b >> 4) & 0xf]);
+                    out.writeByte(HEX_DIGITS[b & 0xf]);
+                }
+            } else {
+                // This character doesn't need encoding. Just copy it over.
+                out.writeUtf8CodePoint(codePoint);
+            }
+        }
+    }
+
+    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict,
+                               boolean plusIsSpace, boolean asciiOnly, Charset charset) {
+        return canonicalize(
+                input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly,
+                charset);
+    }
+
+    static String canonicalize(String input, String encodeSet, boolean alreadyEncoded, boolean strict,
+                               boolean plusIsSpace, boolean asciiOnly) {
+        return canonicalize(
+                input, 0, input.length(), encodeSet, alreadyEncoded, strict, plusIsSpace, asciiOnly, null);
+    }
 
 }
--- app/src/main/java/com/example/lgg/tt/Util.java
+++ app/src/main/java/com/example/lgg/tt/Util.java
@@ -1,9 +1,126 @@
 package com.example.lgg.tt;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Locale;
+
+import androidx.annotation.Nullable;
 
 public class Util {
     public static String format(String format,Object... args){
         return String.format(Locale.US,format,args);
+    }
+
+    private static @Nullable
+    InetAddress decodeIpv6(String input, int pos, int limit) {
+        byte[] address = new byte[16];
+        int b = 0;
+        int compress = -1;
+        int groupOffset = -1;
+
+        for (int i = pos; i < limit; ) {
+            if (b == address.length) return null; // Too many groups.
+
+            // Read a delimiter.
+            if (i + 2 <= limit && input.regionMatches(i, "::", 0, 2)) {
+                // Compression "::" delimiter, which is anywhere in the input, including its prefix.
+                if (compress != -1) return null; // Multiple "::" delimiters.
+                i += 2;
+                b += 2;
+                compress = b;
+                if (i == limit) break;
+            } else if (b != 0) {
+                // Group separator ":" delimiter.
+                if (input.regionMatches(i, ":", 0, 1)) {
+                    i++;
+                } else if (input.regionMatches(i, ".", 0, 1)) {
+                    // If we see a '.', rewind to the beginning of the previous group and parse as IPv4.
+                    if (!decodeIpv4Suffix(input, groupOffset, limit, address, b - 2)) return null;
+                    b += 2; // We rewound two bytes and then added four.
+                    break;
+                } else {
+                    return null; // Wrong delimiter.
+                }
+            }
+
+            // Read a group, one to four hex digits.
+            int value = 0;
+            groupOffset = i;
+            for (; i < limit; i++) {
+                char c = input.charAt(i);
+                int hexDigit = decodeHexDigit(c);
+                if (hexDigit == -1) break;
+                value = (value << 4) + hexDigit;
+            }
+            int groupLength = i - groupOffset;
+            if (groupLength == 0 || groupLength > 4) return null; // Group is the wrong size.
+
+            // We've successfully read a group. Assign its value to our byte array.
+            address[b++] = (byte) ((value >>> 8) & 0xff);
+            address[b++] = (byte) (value & 0xff);
+        }
+
+        // All done. If compression happened, we need to move bytes to the right place in the
+        // address. Here's a sample:
+        //
+        //      input: "1111:2222:3333::7777:8888"
+        //     before: { 11, 11, 22, 22, 33, 33, 00, 00, 77, 77, 88, 88, 00, 00, 00, 00  }
+        //   compress: 6
+        //          b: 10
+        //      after: { 11, 11, 22, 22, 33, 33, 00, 00, 00, 00, 00, 00, 77, 77, 88, 88 }
+        //
+        if (b != address.length) {
+            if (compress == -1) return null; // Address didn't have compression or enough groups.
+            System.arraycopy(address, compress, address, address.length - (b - compress), b - compress);
+            Arrays.fill(address, compress, compress + (address.length - b), (byte) 0);
+        }
+
+        try {
+            return InetAddress.getByAddress(address);
+        } catch (UnknownHostException e) {
+            throw new AssertionError();
+        }
+    }
+
+    private static boolean decodeIpv4Suffix(
+            String input, int pos, int limit, byte[] address, int addressOffset) {
+        int b = addressOffset;
+
+        for (int i = pos; i < limit; ) {
+            if (b == address.length) return false; // Too many groups.
+
+            // Read a delimiter.
+            if (b != addressOffset) {
+                if (input.charAt(i) != '.') return false; // Wrong delimiter.
+                i++;
+            }
+
+            // Read 1 or more decimal digits for a value in 0..255.
+            int value = 0;
+            int groupOffset = i;
+            for (; i < limit; i++) {
+                char c = input.charAt(i);
+                if (c < '0' || c > '9') break;
+                if (value == 0 && groupOffset != i) return false; // Reject unnecessary leading '0's.
+                value = (value * 10) + c - '0';
+                if (value > 255) return false; // Value out of range.
+            }
+            int groupLength = i - groupOffset;
+            if (groupLength == 0) return false; // No digits.
+
+            // We've successfully read a byte.
+            address[b++] = (byte) value;
+        }
+
+        if (b != addressOffset + 4) return false; // Too few groups. We wanted exactly four.
+        return true; // Success.
+    }
+
+    public static int decodeHexDigit(char c) {
+        if (c >= '0' && c <= '9') return c - '0';
+        if (c >= 'a' && c <= 'f') return c - 'a' + 10;
+        if (c >= 'A' && c <= 'F') return c - 'A' + 10;
+        return -1;
     }
 }
